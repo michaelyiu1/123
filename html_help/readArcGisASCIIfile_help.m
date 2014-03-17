490e8e8133da430675e7b33de7c92e9a29afd8a6
George Kourakos <giorgk@gmail.com>
2014-03-18 00:18:37 +0200

    Add html folder

---
+++ html_help/readArcGisASCIIfile_help.m
@@ -0,0 +1,37 @@
+%% readArcGisASCIIfile
+%
+% Reads ARCGIS raster data in ascii format
+%
+% Version : 1.0
+%
+% Author : George Kourakos
+%
+% email: giorgk@gmail.com
+%
+% web : http://groundwater.ucdavis.edu/msim
+%
+% Date 18-Dec_2012
+%
+% Department of Land Air and Water
+%
+% University of California Davis
+%
+%% Usage:
+% [ data  info ] = readArcGisASCIIfile( 'datafile.asc' );
+%
+% or
+%
+% data = readArcGisASCIIfile( 'datafile.asc' );
+%
+%% Input: 
+% filename : The filename needs to be given with the extension
+%
+%% Output: 
+% data : A matrix that contains the data
+%
+% info : A structure with the raster infos
+%
+%      such as # rows and columns, cell size,
+%      coordinates of left lower corner of the grid and
+%      No data value
+%
