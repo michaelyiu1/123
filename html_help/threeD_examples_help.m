1347d28b7c766489a2cae78722154e15b3ca61a1
George Kourakos <giorgk@gmail.com>
2014-08-27 03:11:15 -0700

    add 3D example

---
+++ html_help/threeD_examples_help.m
@@ -0,0 +1,16 @@
+%% Examples in 3D
+% 
+% <msim_help_main.html | main>   <msim_help_demos.html | Tutorials> 
+% <msim_function_categories.html | Functions> <http://www.subsurface.gr | website> |
+%
+% In this section you can find tutorials on how to use mSim functions
+% for simulating steady state groundwater flow in 3D. 
+% 
+% # <threeD_flow1_help.html Example 1> Hexahedral elements, lateral constant flux
+% boundary conditions, general head boundary conditions, heterogeneous hydraulic conductivity 
+%  
+%%
+% 
+% <msim_help_main.html | main>   <msim_help_demos.html | Tutorials> 
+% <msim_function_categories.html | Functions> <http://www.subsurface.gr | website> |
+%
